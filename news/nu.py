1b35f5a3de32298348303f03ac811d0500e91824
Toon Alfrink <toon.alfrink@gmail.com>
2012-05-12 18:38:12 +0200

    updated nu.py for amcat3 framework

--- news/nu.py
+++ news/nu.py
@@ -19,29 +19,13 @@ from __future__ import unicode_literals, print_function, absolute_import
 # License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
 ###########################################################################
 
-from amcat.tools.scraping.processors import GoogleScraper, Form
-from amcat.tools.scraping.objects import HTMLDocument
-from amcat.tools.scraping import toolkit as stoolkit
 
-from amcat.tools import toolkit
-from amcat.models.medium import Medium
+from amcat.scraping.scraper import DBScraper, HTTPScraper
+from amcat.scraping.document import HTMLDocument, IndexDocument
 
-from django import forms
+from urlparse import urljoin
 
-import urllib2
-import logging
-log = logging.getLogger(__name__)
-
-BASE_URL = "http://www.nu.nl/"
-INDEX_URL = BASE_URL + "%(cat)s/%(id)s"
-
-ILLEGAL_URLS = ["http://mobiel.nu.nl/",
-                "http://www.nu.nl/tvgids/",
-                "http://iphone.nu.nl/",
-                "http://www.mobiel.nu.nl",
-                "http://mobi.nu.nl/",
-                "http://m.nu.nl/"]
-
+INDEX_URL = "http://www.nu.nl/"
 MONTHS = [
     'januari',
     'februari',
@@ -57,114 +41,113 @@ MONTHS = [
     'december'
 ]
 
-COMMENTS_PER_NUJIJ_PAGE = 200
+class NuScraper(HTTPScraper, DBScraper):
+    medium_name = "Nu.nl"
 
-class NuForm(Form):
-    date = forms.DateField()
+    def __init__(self, *args, **kwargs):
+        super(NuScraper, self).__init__(*args, **kwargs) 
 
-class NuScraper(GoogleScraper):
-    options_form = NuForm
-    medium = Medium.objects.get(name="nu.nl - website")
 
-    def __init__(self, options):
-        super(NuScraper, self).__init__(options, domain='nu.nl')
-
-    def formatterm(self, date):
-        return "%(day)s %(month)s %(year)s" % dict(
-            day=date.day, month=MONTHS[date.month-1],
-            year=date.year)
-
-    def get(self, art):
-        # Also get comments. Not using CommentScraper because of
-        # GoogleScraper (both alter init).
-        for a in self._get(art):
-            yield a
-
-            comm = art.copy(parent=art)
-            for b in self.comments(comm):
-                yield b 
-
-    def _get(self, art):
-        if not art.props.url.startswith(BASE_URL):
-            return []
-
-        for url in ILLEGAL_URLS:
-            if art.props.url.startswith(url):
-                # URL not allowed
-                return []
-
-        try:
-            datetime = art.doc.cssselect('a.infolink')[0].get('title')
-            datetime = toolkit.readDate(datetime)
-        except:
-            return []
-
-        if datetime.date() != self.options['date']:
-            # Google api can return false positives
-            return []
-
-        # Delete elements not relevant to content
-        for el in ('#photo', '.diablockv1', '.articlebodyad'):
-            try:
-                art.doc.cssselect(el)[0].drop_tree()
-            except:
+    def _get_units(self):
+        """papers are often organised in blocks (pages) of articles, this method gets the blocks, articles are to be gotten later"""
+        index = self.getdoc(INDEX_URL) 
+        menu_options = index.cssselect('ul.listleft li')[1:]
+        index_units = []
+        for unit in menu_options:
+            if "closed" in unit.get('class'):
                 pass
-
-        # Add properties
-        art.props.date = datetime
-        art.props.headline = art.doc.cssselect('.header h1')[0].text_content()
-        art.props.text = art.doc.cssselect('.content')[0]
-        art.props.category = art.props.url.split('/')[3]
-
-        return [art,]
-
-    def comments(self, art):
-        # Search nujij reference
-        nujij = art.doc.cssselect('iframe.NUjijButton')
-
-        if not nujij:
-            log.warn("NuJij button not found for %s" % art.props.url)
-        else:
-            nujij = nujij[0].get('src')
-            nujij = self.getdoc(nujij).cssselect('a')[0].get('href')
-
-            # Loop through all comment pages
-            i = 0
-            while i < 2:
-                startnr = (COMMENTS_PER_NUJIJ_PAGE * i) + 1
-                doc = self.getdoc(nujij + '?pageStart=%s' % startnr)
-
-                comments = doc.cssselect('ol.reacties')
-                if not len(comments):
-                    # Article not yet made
-                    break
+            else:
+                index_units.append(unit)
                 
-                if not len(comments[0].cssselect('.reactie-body')):
-                    # No more comments
-                    break
+        page_int = 0
+        for index_unit in index_units:
+            href = index_unit.cssselect('a')[0].get('href')
+            category = href.strip("/")
+            page_int = page_int + 1
+            yield IndexDocument(url=urljoin(INDEX_URL,href), date=self.options['date'],category = category,page = page_int)
 
-                for comment in comments[0].cssselect('ol > li'):
-                    kop = comment.cssselect('.reactie-kop')[0]
 
-                    try:
-                        minus, plus = kop.cssselect('.reactie-saldo')
-                    except ValueError:
-                        # Comment deleted by moderator
-                        continue
+    def _scrape_unit(self, ipage):
+        """gets articles from an index page"""
+        ipage.prepare(self)
+        #ipage.bytes = "?" what is this?
+        ipage.doc = self.getdoc(ipage.props.url)
 
-                    comm = art.copy()
-                    comm.props.reply_score_minus = minus.text_content()
-                    comm.props.reply_score_plus = plus.text_content()
-                    comm.props.author = kop.cssselect('strong a')[0].text
-                    comm.props.text = comment.cssselect('.reactie-body')[0]
+        
+        #nu.nl articles are not ordered by date anywhere. To be as accurate as possible, all recent articles will be opened, checked for a correct date and then listed 
+        #this only works correctly if the scraped day is less than ~10 hours ago, more if lucky
 
-                    date = kop.cssselect('.tijdsverschil')[0].get('publicationdate')
-                    comm.props.date = toolkit.readDate(date)
+        article_links = []
+        try:
+            for a in ipage.doc.cssselect(".leadarticle h2 a"):
+                article_links.append(urljoin(INDEX_URL,a.get('href')))
+        except IndexError:
+            pass
+        for a in ipage.doc.cssselect(".subarticle h2 a"):
+            article_links.append(urljoin(INDEX_URL,a.get('href')))
+        for a in ipage.doc.cssselect(".list ul li a"):
+            article_links.append(urljoin(INDEX_URL,a.get('href')))
+        for url in article_links:
+            doc = self.getdoc(url)
+            date_str = "{day} {month} {year}".format(day = self.options['date'].day,month = MONTHS[self.options['date'].month-1],year = self.options['date'].year)
+            if "nuzakelijk" in url:
+                if date_str in doc.cssselect("div.smallblock td")[1].text:
+                    page = HTMLDocument(date = self.options['date'],url=url)
+                    page.prepare(self)
+                    page.doc = doc
+                    page = self.get_article(page)
+                    yield page
+                    ipage.addchild(page)
+            else:
 
-                    yield comm
 
-                i += 1
+                try:
+                    if date_str in doc.cssselect(".header div span")[0].text:
+                        page = HTMLDocument(date = self.options['date'],url=url)
+                        page.prepare(self)
+                        page.doc = doc
+                        page = self.get_article(page)
+                        yield page
+                        ipage.addchild(page)
+                except IndexError:
+                    pass
+            
+        yield ipage
+ 
+    def get_article(self, page):
+        def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
+
+        if "nuzakelijk" in page.props.url:
+            page.props.author = page.doc.cssselect("#viawho td")[0].text
+            page.props.headline = page.doc.cssselect("#leadarticle h1")[0].text
+            try:
+                page.doc.cssselect("#leadarticle center")[0].drop_tree()
+                page.doc.cssselect("#leadarticle .footer")[0].drop_tree()
+            except IndexError:
+                pass
+            page.props.text = removeNonAscii(page.doc.cssselect("#articlebody #leadarticle")[0].text_content())
+        else:
+            page.props.author = page.doc.cssselect("#leadarticle .footer .actions span")[0].text
+            page.props.headline = page.doc.cssselect("#leadarticle .header h1")[0].text
+            try:
+                page.doc.cssselect("#leadarticle center")[0].drop_tree()
+                page.doc.cssselect("#leadarticle .footer")[0].drop_tree()
+            except IndexError:
+                pass
+            page.props.text = removeNonAscii(page.doc.cssselect("#leadarticle .content")[0].text_content())
+        
+
+        
+        page.coords = ""
+        return page
+
+
+
 
 if __name__ == '__main__':
     from amcat.scripts.tools import cli
+    from amcat.tools import amcatlogging
+    amcatlogging.debug_module("amcat.scraping.scraper")
+    amcatlogging.debug_module("amcat.scraping.document")
     cli.run_cli(NuScraper)
+    
