7a43e3a3bac21ff3be644dc72f06bd1dcb81eec6
Toon Alfrink <toon.alfrink@gmail.com>
2012-12-17 14:40:39 +0100

    reinvented fd scraper

--- newssites/fd.py
+++ newssites/fd.py
@@ -20,72 +20,121 @@ from __future__ import unicode_literals, print_function, absolute_import
 ###########################################################################
 
 from amcat.scraping.document import Document, HTMLDocument
-import json
-
-from urllib import urlencode
-#from urlparse import urljoin
-#from amcat.tools.toolkit import readDate
-
-LOGIN_URL = "http://fd.nl/handle_login"
-
-SEARCH_URL = "http://fd.nl/solr/select/?wt=json&indent=on&hl=on&hl.fl=title_fd,intro_fd&hl.simple.pre=%3Cspan%20class=%27cbBe2%20fBold%27%3E&hl.simple.post=%3C/span%3E&facet=true&fq=state:published&fq=contenttype:article&q=publication:fd%20publishdate:[{y:04d}-{m:02d}-{d:02d}T00:00:00.000Z%20TO%20{y:04d}-{m:02d}-{d:02d}T23:59:59.999Z]&start=0&rows=9999&sort=publishdate%20desc&facet.field=bron_fd&facet.field=company_fd&facet.field=icb_fd&facet.field=section_fd&facet.field=tag_fd&facet.field=author_fd&facet.field=person_fd&json.wrf=fdmg.fd.search.callback.searchResults&_=1349441426060"
-
-NEXT_URL = "http://fd.nl/?service=searchRedirect&id={}"
-
-from amcat.scraping.scraper import HTTPScraper,DBScraper
+from amcat.scraping.scraper import DBScraper, HTTPScraper
 from amcat.tools.toolkit import readDate
 
+from urllib import urlencode
+import json
+from datetime import timedelta
+
 class WebFDScraper(HTTPScraper, DBScraper):
-    medium_name = "FD Website"
+    medium_name = "website financieel dagblad"
+    initial_url = "http://www.fd.nl"
 
     def __init__(self, *args, **kwargs):
         super(WebFDScraper, self).__init__(*args, **kwargs)
+        self.open(self.initial_url)
 
-    def _login(self,username,password):
-        initial = self.open("http://digikrant.fd.nl")
-        form = {
+
+    login_url = "http://fd.nl/handle_login?{params}" #POST doesn't seem to work
+
+    def _login(self, username, password):
+        login_parameters = {
             'email' : username,
             'password' : password
             }
-        pg = self.open(LOGIN_URL,urlencode(form))
+        params = urlencode(login_parameters)
+        response_json = self.open(self.login_url.format(**locals())).read()
+        response = json.loads(response_json)
+        if response['status'] != "ok":
+            raise ValueError("login status returned not OK but {}".format(response))
 
+
+    
 
     def _get_units(self):
-        index_dict = {
-            'y' : self.options['date'].year,
-            'm' : self.options['date'].month,
-            'd' : self.options['date'].day
-        }
 
+        class Unit(object):
+            def __init__(self, row):
+                self.row = row
+            def __unicode__(self):
+                return unicode(self.row['id'])
 
-        url = SEARCH_URL.format(**index_dict)
-        result = self.open(url) 
-        data = json.loads(result.read()[38:-1])
-        for doc in data['response']['docs']:
-            nxt_url = NEXT_URL.format(doc['objectid'])
-            text = self.open(nxt_url).read()
-            start = text.find('setTimeout("location.href=\'')+27;end = text.find('\';",',start)
-            print("start: {}, end: {}, url: {}".format(start,end,text[start:end]))
-            yield HTMLDocument(url=text[start:end],date=readDate(doc['publishdate']),headline=doc['title'])
+        start = 0
+        result = self.search_result(start)
+        while result['response']['numFound'] > result['response']['start']:
+            for row in result['response']['docs']:
+                yield Unit(row)
+                
+            start += 10
+            result = self.search_result(start)
 
+            
         
-    def _scrape_unit(self, page): 
-        page.prepare(self)
-        page.doc = self.getdoc(page.props.url)
-        page.props.headline = page.doc.cssselect("div.left h1")[0].text_content()
-        page.props.text = page.doc.cssselect("div.left div.left div.left div.topDivider")[0].text_content()
-        page.props.author = page.doc.cssselect("div.left div.left div.left div.left div.left div.fAr14")[0].text.strip()
-        yield page
-        
-        
-        
-        
+    def search_result(self, start):
+        search_url = "http://fd.nl/solr/select/"
 
-    def get_article(self, page):
-        return page
+        today = self.options['date']
+        tomorrow = today + timedelta(days = 1)
+        query_parameters = {
+            'd' : today.day,
+            'm' : today.month,
+            'y' : today.year,
+            'nd' : tomorrow.day,
+            'nm' : tomorrow.month,
+            'ny' : tomorrow.year
+            }
+            
+        request_parameters = {
+            'wt' : 'json',
+            'q' : 'publication:fd publishdate:[{y:04d}-{m:02d}-{d:02d}T00:00:00.000Z TO {ny:04d}-{nm:02d}-{nd:02d}T00:00:00.000Z]'.format(**query_parameters),
+            'start' : start,
+            'fq' : 'contenttype:article',
+            }
+        result_json = self.open(search_url, urlencode(request_parameters)).read()
+        return json.loads(result_json)
+        
+    def _scrape_unit(self, unit):
+        row = unit.row
+        article = HTMLDocument()
+        article.props.headline = row['title']
+        article.props.section = row['home_section_name']
+        article.props.author = row['creator']
+        article.props.date = readDate(row['publishdate'])
+        if 'tag' in row.keys():
+            article.props.tags = ", ".join(row['tag'])
+
+        article.props.url = self.get_article_url(row['objectid'])
+        article.doc = self.getdoc(article.props.url)
+        article.text = article.doc.cssselect("div.left span.article")[0].text_content()
+        
+        for comment in self.get_comments(article.doc):
+            comment.parent = article
+            yield comment
+
+        yield article
+             
 
 
+    redirect_url = "http://fd.nl/?service=searchRedirect&id={artid}"
 
+    def get_article_url(self, artid):
+        script = self.open(self.redirect_url.format(**locals())).read()
+        start = script.find("location.href='") + 15; end = script.find("'", start)
+        return script[start:end]
+
+    def get_comments(self, doc):
+        for div in doc.cssselect("#commentsList div.topDivider"):
+            comment = Document()
+            comment.props.text = div.cssselect("div.wordBreak")[0].text_content()
+            spans = div.cssselect("div.fBold span")
+            try:
+                comment.props.date = readDate(spans[1].text_content().split(" ")[1])
+            except ValueError:
+                comment.props.date = readDate(spans[1].text_content())
+            comment.props.author = spans[0].text_content().strip()
+            yield comment
+            
 
 if __name__ == '__main__':
     from amcat.scripts.tools import cli
