9e1bcb26915b3a8239b8b710dae6a72cbd8a6c97
Toon Alfrink <toon.alfrink@gmail.com>
2013-07-20 20:42:18 +0200

    maintenance: hourly google news scraping

--- maintenance/hourly.sh
+++ maintenance/hourly.sh
@@ -4,10 +4,13 @@ DATE=$(date +'%Y-%m-%d')
 python $PYTHONPATH/scrapers/vienna/orf_at.py $VIENNA_PROJECT $DATE --articleset $ORF_AT_ARTICLESET
 python $PYTHONPATH/scrapers/tv/teletekst.py $TELETEKST_PROJECT --articleset $TELETEKST_ARTICLESET
 python $PYTHONPATH/scrapers/vienna/kurier.py $VIENNA_PROJECT $DATE --articleset $KURIER_ARTICLESET
+python $PYTHONPATH/scrapers/vienna/google_at.py $VIENNA_PROJECT $DATE --articleset $GOOGLE_NEWS_AT_ARTICLESET
 wait
-python $PYTHONPATH/amcat/scripts/maintenance/deduplicate.py $TELETEKST_ARTICLESET
-python $PYTHONPATH/amcat/scripts/maintenance/deduplicate.py $ORF_AT_ARTICLESET
-python $PYTHONPATH/amcat/scripts/maintenance/deduplicate.py $KURIER_ARTICLESET
+DEDUPLICATE='python '$PYTHONPATH'/amcat/scripts/maintenance/deduplicate.py'
+$DEDUPLICATE $TELETEKST_ARTICLESET --first_date $DATE --last_date $DATE
+$DEDUPLICATE $ORF_AT_ARTICLESET --first_date $DATE --last_date $DATE
+$DEDUPLICATE $KURIER_ARTICLESET --first_date $DATE --last_date $DATE
+$DEDUPLICATE $GOOGLE_NEWS_AT_ARTICLESET --first_date $DATE --last_date $DATE
 wait
 kill `ps -ef |grep twitter_statuses_filter|grep -v grep|awk '{print $2}'`
 wait
